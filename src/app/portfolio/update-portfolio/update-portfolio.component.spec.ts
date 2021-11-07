f7fcc068d9e4e3c862a39ec06f0dcce574e0e674
JamalGuyo <jamalguyo08@gmail.com>
2021-11-07 11:21:40 +0300

    FIX: add reactive forms module to updateportfolio module test file to pass test

--- src/app/portfolio/update-portfolio/update-portfolio.component.spec.ts
+++ src/app/portfolio/update-portfolio/update-portfolio.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { UpdatePortfolioComponent } from './update-portfolio.component';
 
@@ -9,6 +10,7 @@ describe('updatePortfolioComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [UpdatePortfolioComponent],
+      imports: [ReactiveFormsModule],
     }).compileComponents();
   });
 
