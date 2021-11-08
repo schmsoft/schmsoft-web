3653f41b0585a9bc2f6c7e726e29549fc80a8975
JamalGuyo <jamalguyo08@gmail.com>
2021-11-08 10:13:12 +0300

    create portfolio details module and components

---
+++ src/app/portfolio/portfolio-detail/portfolio-detail.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PortfolioDetailComponent } from './portfolio-detail.component';
+
+@NgModule({
+  declarations: [PortfolioDetailComponent],
+  imports: [CommonModule],
+  exports: [PortfolioDetailComponent],
+})
+export class PortfolioDetailModule {}
--- src/app/portfolio/portfolio-routing.module.ts
+++ src/app/portfolio/portfolio-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListPortfolioComponent } from './list-portfolio/list-portfolio.component';
+import { PortfolioDetailComponent } from './portfolio-detail/portfolio-detail.component';
 
 const routes: Routes = [
   {
     path: 'portfolio',
     component: ListPortfolioComponent,
+  },
+  {
+    path: 'portfolio/:id',
+    component: PortfolioDetailComponent,
   },
 ];
 
