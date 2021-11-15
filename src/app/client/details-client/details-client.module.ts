9344c44fddbed38ea8afbbb06f28a94be0e00211
JamalGuyo <jamalguyo08@gmail.com>
2021-11-15 11:09:43 +0300

    create tabs to display client details

--- src/app/client/details-client/details-client.module.ts
+++ src/app/client/details-client/details-client.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { DetailsClientComponent } from './details-client.component';
+import { ClrIconModule, ClrTabsModule } from '@clr/angular';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [DetailsClientComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ClrTabsModule, ClrIconModule, RouterModule],
   exports: [DetailsClientComponent],
 })
 export class DetailsClientModule {}
