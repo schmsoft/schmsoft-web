59b254e40018c6fd0a92a1b9e62400f40195a604
JamalGuyo <jamalguyo08@gmail.com>
2021-12-09 16:13:18 +0300

    FIX: test errors - imports

--- src/app/client/details-client/communication-timeline/sms-modal/sms-modal.component.spec.ts
+++ src/app/client/details-client/communication-timeline/sms-modal/sms-modal.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
 
 import { SmsModalComponent } from './sms-modal.component';
 
@@ -9,6 +11,7 @@ describe('SmsModalComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SmsModalComponent],
+      imports: [ToastrModule.forRoot(), FormsModule],
     }).compileComponents();
   });
 
