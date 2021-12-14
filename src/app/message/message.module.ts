a025b16dcc4bb56c042a688f2f2de175e551b466
JamalGuyo <jamalguyo08@gmail.com>
2021-12-14 11:15:40 +0300

    make the sms module reusable

--- src/app/message/message.module.ts
+++ src/app/message/message.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SmsModule } from './sms/sms.module';
 import { MessageRoutingModule } from './message-routing.module';
+import { MessageComponent } from './message.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [MessageComponent],
   imports: [CommonModule, MessageRoutingModule, SmsModule],
 })
 export class MessageModule {}
--- src/app/message/sms/sms.component.ts
+++ src/app/message/sms/sms.component.ts
@@ -1,11 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import {
-  BusinessOwnersGQL,
-  SendSmsToNumbersGQL,
-} from '@graphql/generated/models';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
-import { Subject } from 'rxjs';
-import { map, take, takeUntil, tap } from 'rxjs/operators';
 import { PhoneNumberUtil } from 'google-libphonenumber';
 import { ClrLoadingState } from '@clr/angular';
 
@@ -14,58 +8,30 @@ import { ClrLoadingState } from '@clr/angular';
   templateUrl: './sms.component.html',
   styleUrls: ['./sms.component.scss'],
 })
-export class SmsComponent implements OnInit, OnDestroy {
-  selectedUsers: any[] | undefined;
+export class SmsComponent {
+  @Input()
   users: any = [];
-  message: string = '';
+
+  @Input()
   validateBtnState: ClrLoadingState = ClrLoadingState.DEFAULT;
 
-  destroyed$ = new Subject<boolean>();
+  @Output()
+  handleSendSms = new EventEmitter<{}>();
+
+  @Input()
+  selectedUsers: any[] | undefined;
+
+  @Input()
+  message: string = '';
 
   phoneNumberUtil = PhoneNumberUtil.getInstance();
 
-  constructor(
-    private businessOwnersGQL: BusinessOwnersGQL,
-    private sendSmsToNumbersGQL: SendSmsToNumbersGQL,
-    private toastr: ToastrService
-  ) {}
-
-  ngOnInit(): void {
-    this.businessOwnersGQL
-      .fetch()
-      .pipe(
-        takeUntil(this.destroyed$),
-        map(({ data }) =>
-          data.businessOwners?.map((n) => ({
-            label: n?.user?.name,
-            value: n?.phoneNumber,
-          }))
-        ),
-        tap((data) => {
-          this.users = data;
-        })
-      )
-      .subscribe();
-  }
+  constructor() {}
 
   sendSms() {
     this.validateBtnState = ClrLoadingState.LOADING;
     const phoneNumbers = this.selectedUsers?.map((user) => user?.value);
-    this.sendSmsToNumbersGQL
-      .mutate({
-        phoneNumbers,
-        text: this.message,
-      })
-      .pipe(
-        take(1),
-        tap((resp) => {
-          this.selectedUsers = undefined;
-          this.message = ' ';
-          this.validateBtnState = ClrLoadingState.SUCCESS;
-          this.toastr.success('Message Sent Successfully', 'Hurray!!');
-        })
-      )
-      .subscribe();
+    this.handleSendSms.emit({ phoneNumbers, text: this.message });
   }
 
   inputChange(value: any) {
@@ -80,10 +46,5 @@ export class SmsComponent implements OnInit, OnDestroy {
         this.users = [...this.users, { label: value, value }];
       }
     } catch (e) {}
-  }
-
-  ngOnDestroy() {
-    this.destroyed$.next();
-    this.destroyed$.unsubscribe();
   }
 }
