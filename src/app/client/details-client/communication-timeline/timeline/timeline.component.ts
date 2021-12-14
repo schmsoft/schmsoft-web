fed97d749c1afa9f93a12d001a1aca9f57f3f389
JamalGuyo <jamalguyo08@gmail.com>
2021-12-14 16:31:30 +0300

    FIX:lint error- unused lifecycle method

--- src/app/client/details-client/communication-timeline/timeline/timeline.component.ts
+++ src/app/client/details-client/communication-timeline/timeline/timeline.component.ts
@@ -1,14 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ssw-timeline',
   templateUrl: './timeline.component.html',
   styleUrls: ['./timeline.component.scss'],
 })
-export class TimelineComponent implements OnInit {
+export class TimelineComponent {
   @Input()
   contactRecords: any[] = [];
   constructor() {}
-
-  ngOnInit(): void {}
 }
